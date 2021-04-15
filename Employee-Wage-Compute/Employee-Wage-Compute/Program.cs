06a10a2a489b0640110cbc98f6460abbea247b79
Arjunsing1997 <acchur1997@gmail.com>
2021-04-15 18:32:30 +0530

    UC3-Added Part Time Wage

--- Employee-Wage-Compute/Employee-Wage-Compute/Program.cs
+++ Employee-Wage-Compute/Employee-Wage-Compute/Program.cs
@@ -9,6 +9,7 @@ namespace Employee_Wage_Compute
             Console.WriteLine("Welcome to Employee wage Calculation....");
             // Constant Variable
             int FullTime = 1;                                        //creating a local variable
+            int PartTime = 2;
             int empRatePerHour = 20;
 
             //Local Variables
@@ -16,19 +17,26 @@ namespace Employee_Wage_Compute
             int empWage = 0;
 
             Random random = new Random();                           //Generating Random value
-            int employeeCheck = random.Next(0, 2);       // assigning Random value to the variable 
+            int employeeCheck = random.Next(0, 3);                  // assigning Random value to the variable 
             Console.WriteLine("Random Value: " + employeeCheck);
 
-            if (employeeCheck == FullTime)                 //Checking whether Employee is Present or Not
+
+            if (employeeCheck == FullTime)                          //Checking whether Employee is Present or Not
             {
                 Console.WriteLine("Employee is PRESENT");
                 empHrs = 8;
+            }
+            else if (employeeCheck == PartTime)
+            {
+                Console.WriteLine("Part Time Empoyee: ");
+                empHrs = 4;
             }
             else
             {
                 Console.WriteLine("Employess is ABSENT");
                 empHrs = 0;
             }
+
 
             // computation
             empWage = empRatePerHour * empHrs;
