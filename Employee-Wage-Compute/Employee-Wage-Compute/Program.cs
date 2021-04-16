7579ad0b44fb3ca9fa78da20eab0ca3022bf02b5
Arjunsing1997 <acchur1997@gmail.com>
2021-04-16 18:24:19 +0530

    UC7-Refactored code

--- Employee-Wage-Compute/Employee-Wage-Compute/Program.cs
+++ Employee-Wage-Compute/Employee-Wage-Compute/Program.cs
@@ -4,58 +4,51 @@ namespace Employee_Wage_Compute
 {
     class Program
     {
-
         const int FullTime = 1;                                        //creating a local variable
         const int PartTime = 2;
         public const int maxWorkingDays = 20;
         public const int empRatePerHour = 20;
         public const int maxWorkingHours = 100;
-        static int empHrs = 0;
-        static int empWage = 0;
-        static int workingDays = 1;
-        static int totalEmpWage = 0;
-        static int totalHrs = 0;
-       static  Random random = new Random();                           //Generating Random value
+        static void ComputeWage()
+        {
+            //Local Variables
+            int empHrs = 0, empWage = 0, workingDays = 1, totalEmpWage = 0, totalHrs = 0; 
+
+            Random random = new Random();                           //Generating Random value
+            while (totalHrs < maxWorkingHours && workingDays < maxWorkingDays)
+            {
+                workingDays++;
+                int employeeCheck = random.Next(0, 3);                      // assigning Random value to the variable 
+
+                //USING SWITCH STATEMENT
+                switch (employeeCheck)
+                {
+                    case FullTime:
+                        empHrs = 8;
+                        break;
+
+                    case PartTime:
+                        empHrs = 4;
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empRatePerHour * empHrs;
+                totalHrs += empHrs; //calculting emp hours
+                totalEmpWage += empWage; //calculating emp total wage
+            } //END of WHILE LOOP
+
+            Console.WriteLine("Employee wage for" + workingDays + " day: " + totalEmpWage);
+            Console.WriteLine("Working Hours: " + totalHrs);
+            //Console.Read();
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee wage Calculation....");
- 
-            while (totalHrs < maxWorkingHours && workingDays < maxWorkingDays)
-            {
-                workingDays++;                    
-                EmployeeStatus();                                           //calling method
-                CalculateWage();                                //calling method to calculate wage
-            } 
-
-            Console.WriteLine("Employee wage for" +workingDays+ " day: " +totalEmpWage);
-            Console.WriteLine("Working Hours: " + totalHrs);
-        }
-
-        public static void EmployeeStatus()
-        {
-            int employeeCheck = random.Next(0, 3);                      // assigning Random value to the variable 
-
-            switch (employeeCheck)                                          //USING SWITCH STATEMENT
-            {
-                case FullTime:
-                    empHrs = 8;
-                    break;
-
-                case PartTime:
-                    empHrs = 4;
-                    break;
-
-                default:
-                    empHrs = 0;
-                    break;
-            }
-        }
-        public static void CalculateWage()
-        {
-            empWage = empRatePerHour * empHrs; //calculating Employee wage
-            totalHrs = totalHrs + empHrs; //calculting emp hours
-            totalEmpWage += empWage; //calculating emp total wage
-
+            ComputeWage();
         }
     }
 }
