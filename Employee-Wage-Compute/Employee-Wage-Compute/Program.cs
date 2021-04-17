3072842b88d2effc73db96f44c9d6a705c1cf191
Arjunsing1997 <acchur1997@gmail.com>
2021-04-17 12:44:08 +0530

    UC8-Multiple companies Wage

--- Employee-Wage-Compute/Employee-Wage-Compute/Program.cs
+++ Employee-Wage-Compute/Employee-Wage-Compute/Program.cs
@@ -4,58 +4,51 @@ namespace Employee_Wage_Compute
 {
     class Program
     {
+        public const int FullTime = 1;                                        //creating a local variable
+        public const int PartTime = 2;
+        public static int ComputeWage(string company, int empRatePerHours, int numOfWorkingDays, int maxHrsPerMonth)
+        {
+            //Local Variables
+            int empHrs = 0, totalWorkingDays = 1, totalEmpHrs = 0; 
 
-        const int FullTime = 1;                                        //creating a local variable
-        const int PartTime = 2;
-        public const int maxWorkingDays = 20;
-        public const int empRatePerHour = 20;
-        public const int maxWorkingHours = 100;
-        static int empHrs = 0;
-        static int empWage = 0;
-        static int workingDays = 1;
-        static int totalEmpWage = 0;
-        static int totalHrs = 0;
-       static  Random random = new Random();                           //Generating Random value
+                                       //Generating Random value
+            while (totalEmpHrs < maxHrsPerMonth && totalWorkingDays < numOfWorkingDays)
+            {
+                totalWorkingDays++;
+
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);                      // assigning Random value to the variable 
+
+                //USING SWITCH STATEMENT
+                switch (empCheck)
+                {
+                    case FullTime:
+                        empHrs = 8;
+                        break;
+
+                    case PartTime:
+                        empHrs = 4;
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs; //calculting emp hours
+                Console.WriteLine("Days#: " + totalWorkingDays + " Emp Hours: " + empHrs);
+                
+            } //END of WHILE LOOP
+
+           int totalEmpWage = totalEmpHrs * empRatePerHours;     //calculating emp total wage
+            Console.WriteLine("Total Employee wage for Company : " + company+ " is: " + totalEmpWage);
+            return totalEmpWage;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee wage Calculation....");
- 
-            while (totalHrs < maxWorkingHours && workingDays < maxWorkingDays)
-            {
-                workingDays++;                    
-                EmployeeStatus();                                           //calling method
-                CalculateWage();                                //calling method to calculate wage
-            } 
-
-            Console.WriteLine("Employee wage for" +workingDays+ " day: " +totalEmpWage);
-            Console.WriteLine("Working Hours: " + totalHrs);
-        }
-
-        public static void EmployeeStatus()
-        {
-            int employeeCheck = random.Next(0, 3);                      // assigning Random value to the variable 
-
-            switch (employeeCheck)                                          //USING SWITCH STATEMENT
-            {
-                case FullTime:
-                    empHrs = 8;
-                    break;
-
-                case PartTime:
-                    empHrs = 4;
-                    break;
-
-                default:
-                    empHrs = 0;
-                    break;
-            }
-        }
-        public static void CalculateWage()
-        {
-            empWage = empRatePerHour * empHrs; //calculating Employee wage
-            totalHrs = totalHrs + empHrs; //calculting emp hours
-            totalEmpWage += empWage; //calculating emp total wage
-
+            ComputeWage("Dmart",20,2,10);
+            ComputeWage("Reliance", 10, 4, 20);
         }
     }
 }
