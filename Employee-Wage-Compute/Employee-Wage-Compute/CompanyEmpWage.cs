b641458412398dce2f7b416d76f6cc5bd004684e
Arjunsing1997 <acchur1997@gmail.com>
2021-04-17 16:47:38 +0530

    UC10-multi company wage Using Interface

---
+++ Employee-Wage-Compute/Employee-Wage-Compute/CompanyEmpWage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_Wage_Compute
+{
+    class CompanyEmpWage
+    {
+    }
+}
--- Employee-Wage-Compute/Employee-Wage-Compute/Program.cs
+++ Employee-Wage-Compute/Employee-Wage-Compute/Program.cs
@@ -7,12 +7,10 @@ namespace Employee_Wage_Compute
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee wage Calculation....");
-            EmployeeBuilder Dmart = new EmployeeBuilder("Dmart", 20, 4, 10);  //company name, emp rate per hour,max days,max hours
-            EmployeeBuilder Reliance = new EmployeeBuilder("Reliance", 10, 4, 20);
-            Dmart.ComputeWage();
-            Console.WriteLine(Dmart.toString());
-            Reliance.ComputeWage();
-            Console.WriteLine(Reliance.toString());
+            EmployeeBuilder employeeBuilder = new EmployeeBuilder();
+            employeeBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
+            employeeBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            employeeBuilder.ComputeEmpWage();
         }
     }
 }
